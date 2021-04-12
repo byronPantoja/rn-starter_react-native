45ea9c3d27f78bdb40001a9a8c39ee8ad70708a8
“dbp” <byronpantoja@gmail.com>
2021-04-12 16:09:51 +0800

    cleaning up

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       <Button
-        onPress={() => console.log("Button Pressed")}
+        onPress={() => props.navigation.navigate("Components")}
         title="Go to Components Demo"
       />
-      <TouchableOpacity onPress={() => console.log("List Presses")}>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        onPress={() => props.navigation.navigate("List")}
+        title="Go to List Demo"
+      />
     </View>
   );
 };
