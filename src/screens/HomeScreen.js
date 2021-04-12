4a17148b456dfc06b966d152ba8f8c7c58b1cac3
“dbp” <byronpantoja@gmail.com>
2021-04-12 15:43:25 +0800

    adding a Button and a TouchOpacity to see how they work

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={() => console.log("Button Pressed")}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log("List Presses")}>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
