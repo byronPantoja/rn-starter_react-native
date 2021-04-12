05be6667a7e031f9165ead83f510eeb3f9cbd260
“dbp” <byronpantoja@gmail.com>
2021-04-12 15:30:29 +0800

    adding another variable to nail in the lesson

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,16 +3,16 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
-    { name: "Friend #10" },
+    { name: "Friend #1", age: 18 },
+    { name: "Friend #2", age: 18 },
+    { name: "Friend #3", age: 18 },
+    { name: "Friend #4", age: 18 },
+    { name: "Friend #5", age: 18 },
+    { name: "Friend #6", age: 18 },
+    { name: "Friend #7", age: 18 },
+    { name: "Friend #8", age: 18 },
+    { name: "Friend #9", age: 18 },
+    { name: "Friend #10", age: 18 },
   ];
 
   return (
@@ -20,7 +20,11 @@ const ListScreen = () => {
       keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
       }}
     />
   );
