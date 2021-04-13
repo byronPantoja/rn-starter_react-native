50056d4e575f6c6234b1f561dc990b8d51a87653
“dbp” <byronpantoja@gmail.com>
2021-04-13 09:04:12 +0800

    Starting to learn state management in RN by building a counter demo
    This is like flutter day 1. haha

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const CounterScreen = () => {
+  let counter = 0;
+  return (
+    <View>
+      <Button title="Increase" />
+      <Button title="Decrease" />
+      <Text>Current Count:</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
